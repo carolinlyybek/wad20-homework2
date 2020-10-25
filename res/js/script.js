3654950d5bc2c40753a109d1d614b74955d9f268
KristiinaKr <krausekristiina@gmail.com>
2020-10-25 18:38:35 +0200

    proovin js-iga ja html loogikat muuta

--- res/js/script.js
+++ res/js/script.js
@@ -1,5 +1,18 @@
-$ (document).ready(function() {    
-    $("document").on("click", ".avatar", function() {
+
+function myFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+$(function(){
+    /* $("document").on("click", ".avatar", function() {
         $(".dropdown").toggleClass("visible");
+    });
+*/
+    function myFunction() {
+        document.getElementById("dropdown").classList.toggle("show");
+    }
+
+    $(".avatar-container").click(function (){
+        $(".dropdown").toggle();
     });
 });
