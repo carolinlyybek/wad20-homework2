2f3bd25dc119671150afeb906fcf8d291e9388e0
Carolin Lüübek <lyybek.carolin@gmail.com>
2020-10-25 21:13:51 +0200

    some more user fetching, not working

--- res/js/script.js
+++ res/js/script.js
@@ -5,42 +5,46 @@ $(function(){
     });
 
     loadUserInfo()
-        return $.get (
-            {
-                url: 'https://wad20postit.docs.apiary.io/#reference/0/posts-collection/get-all-posts',
-            success: function (response) {
-                    return response
-            },
-                error: function () {
-                    alert('error')
-                }
-            },
-            displayUserInfo(user)
-            /*see vb ei pea siin olema aga proovisin */
-        );
-
-
-    function displayUserInfo(user) {
-        $('#firstname').text(user.firstname + " " + user.lastname);
-        $('#email').text(user.email );
-    }
-});
-
-/* mul on praxist selline variant
-
-loadUserInfo()
-    .then(function (response) {
-        let user = new User(
-            response.firstname,
-            response.lastname,
-            response.birthdate,
-            response.faculty
-        );
+        .then(function(response) {
+            let user = new User(
+                response.firstname,
+                response.lastname,
+                response.email,
+                response.avatar
+            );
 
         displayUserInfo(user)
     })
     .catch(function () {
         alert('Error loading user info')
     });
-*/
+
+    function displayUserInfo(user) {
+        $('#name').text(user.firstname + " " + user.lastname);
+        $('#email').text(user.email);
+        $('#avatar').url(user.avatar);
+    }
+
+    function loadUserInfo() {
+        return $.get (
+            {
+                url: 'https://private-anon-293f083657-wad20postit.apiary-mock.com/users/1',
+                success: function (response) {
+                    return response
+                },
+                error: function () {
+                    alert('error')
+                }
+            }
+            /*displayUserInfo(user)*/
+            /*see vb ei pea siin olema aga proovisin */
+        );
+    }
+
+});
+
+/* mul on praxist selline variant */
+
+
+
 
