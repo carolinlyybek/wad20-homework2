17e81c5098aa55934a0988c5325720ec66b0f613
KristiinaKr <krausekristiina@gmail.com>
2020-10-25 20:03:47 +0200

    dropdown works, started with json logic

--- res/js/script.js
+++ res/js/script.js
@@ -1,18 +1,46 @@
-
-function myFunction() {
-    document.getElementById("dropdown").classList.toggle("show");
-}
-
 $(function(){
-    /* $("document").on("click", ".avatar", function() {
-        $(".dropdown").toggleClass("visible");
-    });
-*/
-    function myFunction() {
-        document.getElementById("dropdown").classList.toggle("show");
-    }
 
     $(".avatar-container").click(function (){
         $(".dropdown").toggle();
     });
+
+    loadUserInfo()
+        return $.get (
+            {
+                url: 'https://wad20postit.docs.apiary.io/#reference/0/posts-collection/get-all-posts',
+            success: function (response) {
+                    return response
+            },
+                error: function () {
+                    alert('error')
+                }
+            },
+            displayUserInfo(user)
+            /*see vb ei pea siin olema aga proovisin */
+        );
+
+
+    function displayUserInfo(user) {
+        $('#firstname').text(user.firstname + " " + user.lastname);
+        $('#email').text(user.email );
+    }
 });
+
+/* mul on praxist selline variant
+
+loadUserInfo()
+    .then(function (response) {
+        let user = new User(
+            response.firstname,
+            response.lastname,
+            response.birthdate,
+            response.faculty
+        );
+
+        displayUserInfo(user)
+    })
+    .catch(function () {
+        alert('Error loading user info')
+    });
+*/
+
